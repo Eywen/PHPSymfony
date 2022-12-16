5d3a8f7089cb462fa7d5e0b515d2d9567255a3ef
Kara <blk20100@gmail.com>
2022-12-16 20:43:34 +0100

    Adding result controllers querys class and commands. # 2

---
+++ src/Controller/ApiResultsCommandInterface.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * Class ApiResultsController
+ *
+ * @package App\Controller
+ *
+ */
+interface ApiResultsCommandInterface
+{
+    /**
+     * DELETE Action<br>
+     * Summary: Removes the Result resource.<br>
+     * Notes: Deletes the result identified by &#x60;resultId&#x60;.
+     *
+     * @param int $resultId User id
+     */
+    public function deleteAction(Request $request, int $resultId): Response;
+
+    /**
+     * POST action<br>
+     * Summary: Creates a Result resource.
+     *
+     * @param Request $request request
+     */
+    public function postAction(Request $request): Response;
+
+    /**
+     * PUT action<br>
+     * Summary: Updates the Result resource.<br>
+     * Notes: Updates the result identified by &#x60;resultId&#x60;.
+     *
+     * @param Request $request request
+     * @param int $resultId User id
+     */
+    public function putAction(Request $request, int $resultId): Response;
+}
